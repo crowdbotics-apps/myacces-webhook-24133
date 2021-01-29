2d8201df263c6d7bb97f3d6e04fbcdf0c90ffbe7
Crowdbotics <team@crowdbotics.com>
2021-01-29 11:02:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/myacces_webhook_24133/MainActivity.java
@@ -0,0 +1,15 @@
+package com.myacces_webhook_24133;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "myacces_webhook_24133";
+    }
+}
